1f5e1a01291b80d4f434d3cdd05002ed91c438e9
Mohit Anand <62118896+mohit-1501@users.noreply.github.com>
2020-10-01 02:17:09 +0530

    Add files via upload

---
+++ Sorting/insertion-descending.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    int i;
+    for(i=0;i<n;i++)
+    cin >> arr[i];
+
+    for(int j=1;j<n;j++)
+    {
+        int key=arr[j];
+        int p=j-1;
+        while(p>=0 && arr[p]<key){
+            arr[p+1]=arr[p];
+            p=p-1;
+            arr[p+1]=key;
+        }
+    }
+    for(i=0;i<n;i++)
+    cout << arr[i] << " ";
+    return 0;
+}
